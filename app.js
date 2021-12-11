0db63d4d96cd5d6894e43b4ad8ea8273edb9b2ce
thasup <thanachonfirst@hotmail.com>
2021-12-12 04:00:02 +0700

    Set database schema

--- app.js
+++ app.js
@@ -4,5 +4,41 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/fruitDB');
+  await mongoose.connect('mongodb://localhost:27017/fruitDB', {useNewUrlParser: true});
 };
+
+// Create fruit schema
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit (
+    {
+        name: "Apple",
+        rating: 8,
+        review: "Great fruit"
+    }
+);
+
+// fruit.save();
+
+// Create person schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const people = new Person (
+    {
+        name: "John",
+        age: 37
+    }
+);
+
+// people.save();
