7fb87a15cdfe9fa0664279d8452e3d182da3a648
thasup <thanachonfirst@hotmail.com>
2021-12-12 02:45:21 +0700

    Add fruit collection to fruitDB

--- app.js
+++ app.js
@@ -17,6 +17,51 @@ client.connect((err) => {
 
     const db = client.db(dbName);
 
-    client.close();
+    findDocuments(db, ()=> {
+        client.close();
+    });
 });
 
+const insertDocuments = (db, callback) => {
+    // Get the documents collection
+    const collection = db.collection("fruits");
+    // Insert some documents
+    collection.insertMany(
+        [
+            {
+                name: "Apple",
+                score: 8,
+                review: "Great fruit",
+            }, 
+            {
+                name: "Orange",
+                score: 6,
+                review: "Kinda sour",
+            }, 
+            {
+                name: "Banana",
+                score: 9,
+                review: "Wonderful stuff!",
+            }
+        ],
+        (err, result) => {
+            assert.equal(err, null);
+            assert.equal(3,result.insertedCount);
+            assert.equal(3,Object.keys(result.insertedIds).length);
+            console.log("Inserted 3 documents into the collection");
+            callback(result);
+        }
+    );
+};
+
+const findDocuments = (db, callback) => {
+    // Get the documents collection
+    const collection = db.collection("fruits");
+    // Find some documents
+    collection.find({}).toArray((err, fruits) => {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(fruits);
+        callback(fruits);
+    });
+};
